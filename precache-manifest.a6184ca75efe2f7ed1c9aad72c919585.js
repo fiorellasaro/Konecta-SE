5ea67af8fd34a466c2b7c79d559c0b39e45a66c2
Jesseliz <choque101108@gmail.com>
2020-01-30 16:51:47 -0500

    Initial dist subtree commit

---
+++ precache-manifest.a6184ca75efe2f7ed1c9aad72c919585.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db6bbc8d26a60ae672aa",
+    "url": "/konecta-se/css/app.27d295e2.css"
+  },
+  {
+    "revision": "865bbb3aac51546ad40f",
+    "url": "/konecta-se/css/chunk-3811a6f1.31697e05.css"
+  },
+  {
+    "revision": "2ebaf82e220974f3138c",
+    "url": "/konecta-se/css/chunk-4c9fc32e.d20e514f.css"
+  },
+  {
+    "revision": "8fe963a8062b3b100558",
+    "url": "/konecta-se/css/chunk-63e37f00.076ea886.css"
+  },
+  {
+    "revision": "0530af78b6fa954389fd",
+    "url": "/konecta-se/css/chunk-6ff7380a.5b3f49a4.css"
+  },
+  {
+    "revision": "27b389e4b078443b8d17",
+    "url": "/konecta-se/css/chunk-vendors.bd0b5962.css"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/konecta-se/fonts/MaterialIcons-Regular.0509ab09.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/konecta-se/fonts/MaterialIcons-Regular.29b882f0.woff"
+  },
+  {
+    "revision": "96c476804d7a788cc1c05351b287ee41",
+    "url": "/konecta-se/fonts/MaterialIcons-Regular.96c47680.eot"
+  },
+  {
+    "revision": "da4ea5cdfca6b3baab285741f5ccb59f",
+    "url": "/konecta-se/fonts/MaterialIcons-Regular.da4ea5cd.ttf"
+  },
+  {
+    "revision": "059bfd7d235efbb03257de081f45d3a2",
+    "url": "/konecta-se/img/01.059bfd7d.png"
+  },
+  {
+    "revision": "82c2c2d8cb4a2a3fc6a997ba71117dac",
+    "url": "/konecta-se/img/02.82c2c2d8.png"
+  },
+  {
+    "revision": "638bc90b79d1274fdc90b6873da500e0",
+    "url": "/konecta-se/img/04.638bc90b.png"
+  },
+  {
+    "revision": "3bea8a84d8ad144d3fcda374ed207d5a",
+    "url": "/konecta-se/img/05.3bea8a84.png"
+  },
+  {
+    "revision": "354eb7d9ddb9ae88e554fcb4840d9ba2",
+    "url": "/konecta-se/img/06.354eb7d9.png"
+  },
+  {
+    "revision": "81e28f7397caa0af5dd3af937df9bfe0",
+    "url": "/konecta-se/img/konecta-b.81e28f73.png"
+  },
+  {
+    "revision": "d151b3b6ec3475e6f64a29505663613e",
+    "url": "/konecta-se/img/section-1.d151b3b6.png"
+  },
+  {
+    "revision": "7ffa70fd49c1a0de4a0712a67da32ab1",
+    "url": "/konecta-se/img/section-2.7ffa70fd.png"
+  },
+  {
+    "revision": "f87d70bb79d3d2936214b877152f067c",
+    "url": "/konecta-se/index.html"
+  },
+  {
+    "revision": "db6bbc8d26a60ae672aa",
+    "url": "/konecta-se/js/app.da4976f9.js"
+  },
+  {
+    "revision": "4e452e69c79d6d2561ea",
+    "url": "/konecta-se/js/chunk-2d22d746.379c484c.js"
+  },
+  {
+    "revision": "865bbb3aac51546ad40f",
+    "url": "/konecta-se/js/chunk-3811a6f1.4ae71e2f.js"
+  },
+  {
+    "revision": "2ebaf82e220974f3138c",
+    "url": "/konecta-se/js/chunk-4c9fc32e.dae9788d.js"
+  },
+  {
+    "revision": "8fe963a8062b3b100558",
+    "url": "/konecta-se/js/chunk-63e37f00.79a862ad.js"
+  },
+  {
+    "revision": "0530af78b6fa954389fd",
+    "url": "/konecta-se/js/chunk-6ff7380a.4f5b51f5.js"
+  },
+  {
+    "revision": "e8d08e69ef76310b9fa3",
+    "url": "/konecta-se/js/chunk-720f1721.b6bb24c5.js"
+  },
+  {
+    "revision": "27b389e4b078443b8d17",
+    "url": "/konecta-se/js/chunk-vendors.dd971ab3.js"
+  },
+  {
+    "revision": "bba683fecf01afc2971cc5302b544bbe",
+    "url": "/konecta-se/k-azul.png"
+  },
+  {
+    "revision": "8b755839a0d6ae8862e2505dfefda651",
+    "url": "/konecta-se/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/konecta-se/robots.txt"
+  }
+]);
